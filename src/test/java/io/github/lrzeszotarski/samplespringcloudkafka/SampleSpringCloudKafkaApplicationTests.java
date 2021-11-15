b6475a2fad536d30d3da8c1f877efd48e235918e
Lukasz Rzeszotarski <lrzeszotarski@MacBook-Pro-von-Lukasz.local>
2021-11-15 22:19:24 +0100

    init

---
+++ src/test/java/io/github/lrzeszotarski/samplespringcloudkafka/SampleSpringCloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lrzeszotarski.samplespringcloudkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringCloudKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
