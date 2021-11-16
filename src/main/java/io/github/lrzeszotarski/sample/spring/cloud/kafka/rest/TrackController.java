f777ada4c959efe499327b7cb18527491efc1c98
Lukasz Rzeszotarski <lrzeszotarski@MacBook-Pro-von-Lukasz.local>
2021-11-16 01:37:48 +0100

    init

--- src/main/java/io/github/lrzeszotarski/sample/spring/cloud/kafka/rest/TrackController.java
+++ src/main/java/io/github/lrzeszotarski/sample/spring/cloud/kafka/rest/TrackController.java
@@ -2,9 +2,14 @@ package io.github.lrzeszotarski.sample.spring.cloud.kafka.rest;
 
 import io.github.lrzeszotarski.sample.spring.cloud.kafka.rest.dto.Track;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+import java.util.UUID;
 
 @RestController
 public class TrackController {
@@ -17,7 +22,12 @@ public class TrackController {
 
     @PostMapping("/tracks")
     public Track freeTrack(@RequestBody Track freeTrack) {
-        streamBridge.send("tracks.free", freeTrack);
+        final Message<Track> trackMessage = MessageBuilder
+                .withPayload(freeTrack)
+                .setHeader("partitionKey", UUID.randomUUID().toString())
+                .setHeader("kafka_messageKey", Objects.requireNonNull(UUID.randomUUID().toString()))
+                .build();
+        streamBridge.send("tracks.free", trackMessage);
         return freeTrack;
     }
 }
